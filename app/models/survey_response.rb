cc218e09fc6861df581e1a94e672a02c32a275ff
Jose Santiago Jr <jose@binary-dev.com>
2013-12-02 11:36:47 -0500

    Added popup on survey submission to allow resetting without user intervention

--- app/models/survey_response.rb
+++ app/models/survey_response.rb
@@ -1,6 +1,7 @@
 class SurveyResponse < ActiveRecord::Base
   belongs_to :survey
   attr_accessible :survey_responses, :survey_id
+  attr_accessor :kiosk_mode
   serialize :survey_responses, ActiveRecord::Coders::Hstore
   validates_presence_of :survey_id
 end
